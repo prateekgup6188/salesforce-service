be436abb9a02b71f1764df90ed545a4ad682471a
prateekgup6188 <prateekgupta6188@gmail.com>
2025-03-17 03:03:13 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,12 +1,9 @@
 const { Pool } = require('pg');
+const { connectionString } = require('pg/lib/defaults');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.POSTGRESQL_USERNAME,
-  host: process.env.POSTGRESQL_HOST,
-  database: process.env.POSTGRESQL_DATABASE,
-  password: process.env.POSTGRESQL_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
